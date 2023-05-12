1e69ff2c68cae4f74c59d7652660beb8ac73e3d2
DarkXRaven <mysqli96@gmail.com>
2023-05-12 18:51:18 +0000

    gcc

---
+++ lib/gcc/aarch64-elf/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-10.2.0/configure --target=aarch64-elf --prefix=/home/geist/svn/toolchains/aarch64-elf-10.2.0-Linux-x86_64 --enable-languages=c,c++ --disable-werror";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
